fee1853a095c85013a21046321e6a4f4e4192b42
Oleg Elifantiev <oleg@elifantiev.ru>
2015-08-18 22:42:37 +0300

    OpenShift compatibility

--- server.js
+++ server.js
@@ -192,7 +192,10 @@ poll(function(updates){
    });
 });
 
-app.listen(process.env.PORT);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address);
 
 // Keep alive every 5 min
 var http = require("http");
